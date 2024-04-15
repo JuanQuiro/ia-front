e1a51f3a51fe28900559ea3871c126eb98d18a20
Juan Quiro <juanquirozsanchez@hotmail.com>
2024-04-14 20:18:19 -0400

    actualizacion 1

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = "http://localhost:3000";
+const backendUrl = "https://ia-back.vercel.app/";
 
 const ChatContext = createContext();
 
